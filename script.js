1144ab0568e41f319d5f934f3ff88a4d5a8bb4d5
SI siam <shahidulmd392@gmail.com>
2021-01-28 15:52:50 +0600

    second Commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// Addition Iphone Event handler 
+let addIphone = document.getElementById("sellPlus");
+addIphone.addEventListener("click", () => {
+    let count = document.getElementById("countPlus").value;
+    count;
+    console.log(count);
+})
+
